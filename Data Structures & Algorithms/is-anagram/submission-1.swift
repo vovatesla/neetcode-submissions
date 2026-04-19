644be431e99a3d4dba50794bccf2cb520060813e
vovatesla <110394032+vovatesla@users.noreply.github.com>
2026-04-19 16:15:30 +0300

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        var sDict = [Character:Int]()
+        var tDict = [Character:Int]()
+        guard sDict.count == tDict.count else {
+            return false
+        }
+
+        for letter in s {
+            if sDict[letter] == nil {
+                sDict[letter] = 1
+            } else {
+                sDict[letter]! += 1
+            }
+        }
+        
+        for letter in t {
+            if tDict[letter] == nil {
+                tDict[letter] = 1
+            } else {
+                tDict[letter]! += 1
+            }
+        }
+        
+        return sDict == tDict
+    }
+}
